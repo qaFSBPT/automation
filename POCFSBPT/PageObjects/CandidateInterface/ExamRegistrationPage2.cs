a6f12cea3953d8280e8e3c8a1d1518016d7b693f
fsbpt-2003-dom\jmurdough <jmurdough@ceiamerica.com>
2019-07-01 14:53:30 -0400

    Today's changes

---
+++ POCFSBPT/PageObjects/CandidateInterface/ExamRegistrationPage2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCFSBPT.PageObjects
+{
+    class ExamRegistrationPage2
+    {
+        private IWebDriver driver;
+
+        private string pageURL = ConfigurationManager.AppSettings["CandidateInterface"] + "/SPA/ExamRegistration#select-event-date";
+
+        public ExamRegistrationPage2(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        // Page Elements
+        #region PageElements
+
+        [FindsBy(How = How.Id, Using = "JurisdictionId")]
+        private IWebElement jurisdictionDropdown;
+        private SelectElement selectJurisdiction
+        {
+            get
+            {
+                return new SelectElement(jurisdictionDropdown);
+            }
+        }
+
+        #endregion PageElements
+
+        // Page Functions
+        #region PageFunctions
+        public void SelectJurisdiction(string jurisdiction)
+        {
+            selectJurisdiction.SelectByText(jurisdiction);
+        }
+
+        #endregion PageFunctions
+    }
+}
